1025ee9c0cdf225a201c78e9d38db1eb15d57150
Keiho Sakapon <github@saka-pon.net>
2014-02-11 22:22:15 +0900

    Add DisposableBaseTest.

---
+++ KLibrary2_1/UnitTest/ComponentModel/DisposableBaseTest.cs
@@ -0,0 +1,20 @@
+﻿using KLibrary.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest.ComponentModel
+{
+    [TestClass]
+    public class DisposableBaseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    public class Disposable1 : DisposableBase
+    {
+
+    }
+}
