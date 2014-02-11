047a41a38be639bae0705d226ae7d56326ab955a
Keiho Sakapon <github@saka-pon.net>
2014-02-11 22:02:42 +0900

    Implement DisposableBase class.

--- KLibrary2_1/ComponentModel/ComponentModel/DisposableBase.cs
+++ KLibrary2_1/ComponentModel/ComponentModel/DisposableBase.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace KLibrary.ComponentModel
 {
-    class DisposableBase
+    public abstract class DisposableBase : IDisposable
     {
+        ~DisposableBase()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // マネージ リソースを解放します。
+            }
+
+            // アンマネージ リソースを解放します。
+        }
     }
 }
