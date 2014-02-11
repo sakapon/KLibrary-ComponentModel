c73d29151c0cd6ed2ed42392b59e99e26348d354
Keiho Sakapon <github@saka-pon.net>
2014-02-11 18:11:39 +0900

    Update project settings.

--- KLibrary2_1/Properties/SharedAssemblyInfo.cs
+++ KLibrary2_1/Properties/SharedAssemblyInfo.cs
@@ -1,30 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("KLibrary.ComponentModel")]
-[assembly: AssemblyDescription("A library for component model.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Keiho Sakapon")]
 [assembly: AssemblyProduct("KLibrary")]
 [assembly: AssemblyCopyright("© 2014 Keiho Sakapon.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyMetadata("ProjectUrl", "https://github.com/sakapon/KLibrary-ComponentModel")]
-[assembly: AssemblyMetadata("LicenseUrl", "https://github.com/sakapon/KLibrary-ComponentModel")]
-[assembly: AssemblyMetadata("Tags", "Component")]
-[assembly: AssemblyMetadata("ReleaseNotes", "")]
-
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
-// その型の ComVisible 属性を true に設定してください。
-[assembly: ComVisible(false)]
-
-// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
-[assembly: Guid("78538c5f-eac2-4084-95a6-51ad56a43275")]
 
 // アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
 //
