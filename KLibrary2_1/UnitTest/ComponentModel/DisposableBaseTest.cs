0206e0efaea3784ab846550237e0cff58c21efa8
Keiho Sakapon <github@saka-pon.net>
2014-02-12 00:02:45 +0900

    Add test methods for DisposableBase.

--- KLibrary2_1/UnitTest/ComponentModel/DisposableBaseTest.cs
+++ KLibrary2_1/UnitTest/ComponentModel/DisposableBaseTest.cs
@@ -8,13 +8,74 @@ namespace UnitTest.ComponentModel
     public class DisposableBaseTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Dispose_True1()
         {
+            var isManagedResourceReleased = false;
+            var isUnmanagedResourceReleased = false;
+
+            using (var obj = new Disposable1())
+            {
+                obj.ReleaseManagedResource = () => isManagedResourceReleased = true;
+                obj.ReleaseUnmanagedResource = () => isUnmanagedResourceReleased = true;
+            }
+
+            Assert.AreEqual(true, isManagedResourceReleased);
+            Assert.AreEqual(true, isUnmanagedResourceReleased);
+        }
+
+        [TestMethod]
+        public void Dispose_True2()
+        {
+            var isManagedResourceReleased = false;
+            var isUnmanagedResourceReleased = false;
+
+            var obj = new Disposable1();
+            obj.ReleaseManagedResource = () => isManagedResourceReleased = true;
+            obj.ReleaseUnmanagedResource = () => isUnmanagedResourceReleased = true;
+            obj.Dispose();
+
+            Assert.AreEqual(true, isManagedResourceReleased);
+            Assert.AreEqual(true, isUnmanagedResourceReleased);
+        }
+
+        [TestMethod]
+        public void Dispose_False()
+        {
+            var isManagedResourceReleased = false;
+            var isUnmanagedResourceReleased = false;
+
+            var obj = new Disposable1();
+            obj.ReleaseManagedResource = () => isManagedResourceReleased = true;
+            obj.ReleaseUnmanagedResource = () => isUnmanagedResourceReleased = true;
+
+            Assert.AreEqual(false, isManagedResourceReleased);
+            Assert.AreEqual(false, isUnmanagedResourceReleased);
+
+            Assert.Inconclusive("デバッグして、Disposable1.Dispose メソッドを目視で確認してください。");
         }
     }
 
     public class Disposable1 : DisposableBase
     {
+        public Action ReleaseManagedResource { get; set; }
+        public Action ReleaseUnmanagedResource { get; set; }
 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // マネージ リソースを解放します。
+                if (ReleaseManagedResource != null)
+                {
+                    ReleaseManagedResource();
+                }
+            }
+
+            // アンマネージ リソースを解放します。
+            if (ReleaseUnmanagedResource != null)
+            {
+                ReleaseUnmanagedResource();
+            }
+        }
     }
 }
