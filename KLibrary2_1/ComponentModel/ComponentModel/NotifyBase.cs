f223b86a20c8291e27328136adcf3ed10bacb518
Keiho Sakapon <github@saka-pon.net>
2014-02-12 00:47:12 +0900

    Implement basic NotifyBase class.

--- KLibrary2_1/ComponentModel/ComponentModel/NotifyBase.cs
+++ KLibrary2_1/ComponentModel/ComponentModel/NotifyBase.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace KLibrary.ComponentModel
 {
-    class NotifyBase
+    public abstract class NotifyBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged = (o, e) => { };
+
+        public void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void AddPropertyChangedHandler(string propertyName, Action action)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+
+            PropertyChanged += (o, e) =>
+            {
+                if (e.PropertyName == propertyName)
+                {
+                    action();
+                }
+            };
+        }
     }
 }
---
+++ KLibrary2_1/UnitTest/ComponentModel/NotifyBaseTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.ComponentModel
+{
+    [TestClass]
+    public class NotifyBaseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
