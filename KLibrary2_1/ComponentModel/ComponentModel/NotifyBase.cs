6be3e70114ff57c1fb70028e0c79580a5eaea3bf
Keiho Sakapon <github@saka-pon.net>
2014-02-14 18:09:42 +0900

    Add comments.

--- KLibrary2_1/ComponentModel/ComponentModel/NotifyBase.cs
+++ KLibrary2_1/ComponentModel/ComponentModel/NotifyBase.cs
@@ -6,15 +6,30 @@ using System.Runtime.CompilerServices;
 
 namespace KLibrary.ComponentModel
 {
+    /// <summary>
+    /// Provides the base implementation for observable objects.
+    /// </summary>
     public abstract class NotifyBase : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Occurs when a property value has changed.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged = (o, e) => { };
 
+        /// <summary>
+        /// Notifies that the property value has changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property which has changed.</param>
         public void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
         {
             PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        /// <summary>
+        /// Add the action which is executed when the property has changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property which has changed.</param>
+        /// <param name="action">The action which is executed when the property has changed.</param>
         public void AddPropertyChangedHandler(string propertyName, Action action)
         {
             if (action == null) throw new ArgumentNullException("action");
