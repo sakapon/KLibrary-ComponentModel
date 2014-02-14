3022424fa6e20b02da3dbbe810021c450fb87de5
Keiho Sakapon <github@saka-pon.net>
2014-02-14 17:41:36 +0900

    Add comments.

--- KLibrary2_1/ComponentModel/ComponentModel/DisposableBase.cs
+++ KLibrary2_1/ComponentModel/ComponentModel/DisposableBase.cs
@@ -2,27 +2,40 @@
 
 namespace KLibrary.ComponentModel
 {
+    /// <summary>
+    /// Provides the base implementation for disposable objects.
+    /// </summary>
     public abstract class DisposableBase : IDisposable
     {
+        /// <summary>
+        /// Finalize this object.
+        /// </summary>
         ~DisposableBase()
         {
             Dispose(false);
         }
 
+        /// <summary>
+        /// Releases all resources used by this object.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Releases the managed and/or unmanaged resources used by this object.
+        /// </summary>
+        /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
             {
-                // マネージ リソースを解放します。
+                // Releases the managed resources.
             }
 
-            // アンマネージ リソースを解放します。
+            // Releases the unmanaged resources.
         }
     }
 }
