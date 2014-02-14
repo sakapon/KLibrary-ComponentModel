b557fdde8074f2d2ad743f75422f5fb0c6d52e9e
Keiho Sakapon <github@saka-pon.net>
2014-02-15 01:56:08 +0900

    Make UnitTest be the friend assembly.

--- KLibrary2_1/ComponentModel/Properties/AssemblyInfo.cs
+++ KLibrary2_1/ComponentModel/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
@@ -21,3 +22,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("78538c5f-eac2-4084-95a6-51ad56a43275")]
 
 [assembly: CLSCompliant(true)]
+
+// Retrieve the full public key from a strong-named key (.snk) file.
+// http://msdn.microsoft.com/library/system.runtime.compilerservices.internalsvisibletoattribute.internalsvisibletoattribute.aspx
+[assembly: InternalsVisibleTo("UnitTest, PublicKey=00240000048000009400000006020000002400005253413100040000010001008b0b344823f8497310ac8403d1296157615ef552c2a41fd0f920f4515fb21c64f1e86397dcc0edb4d10acc168cc783efa8b18d7a97a8fbd7e52998cbfc88a1710c8dca448c8b2d1670b1ecefcc488eb079ca6dc34915cabe4145eb1a940bd59453bb8c27a818d7fef6e2c8a6bd5ab11b54cce7bc24c6ede91d8a03f74aafc1e8")]
---
+++ KLibrary2_1/UnitTest/TypeHelperTest.cs
@@ -0,0 +1,29 @@
+﻿using KLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TypeHelperTest
+    {
+        [TestMethod]
+        public void GetDefaultValue_1()
+        {
+            Assert.AreEqual(default(bool), typeof(bool).GetDefaultValue());
+            Assert.AreEqual(default(byte), typeof(byte).GetDefaultValue());
+            Assert.AreEqual(default(short), typeof(short).GetDefaultValue());
+            Assert.AreEqual(default(int), typeof(int).GetDefaultValue());
+            Assert.AreEqual(default(long), typeof(long).GetDefaultValue()); // 0L
+            Assert.AreEqual(default(char), typeof(char).GetDefaultValue()); // '\0'
+            Assert.AreEqual(default(float), typeof(float).GetDefaultValue()); // 0F
+            Assert.AreEqual(default(double), typeof(double).GetDefaultValue()); // 0D
+            Assert.AreEqual(default(IntPtr), typeof(IntPtr).GetDefaultValue());
+            Assert.AreEqual(default(DayOfWeek), typeof(DayOfWeek).GetDefaultValue());
+            Assert.AreEqual(default(decimal), typeof(decimal).GetDefaultValue()); // 0M
+            Assert.AreEqual(default(DateTime), typeof(DateTime).GetDefaultValue());
+            Assert.AreEqual(default(object), typeof(object).GetDefaultValue());
+            Assert.AreEqual(default(string), typeof(string).GetDefaultValue());
+        }
+    }
+}
