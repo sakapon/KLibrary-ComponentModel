9a53fa872ca34f1008a71095876f578a5aaf4ad1
Keiho Sakapon <github@saka-pon.net>
2014-02-15 01:43:31 +0900

    Add TypeHelper class.

---
+++ KLibrary2_1/ComponentModel/TypeHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLibrary
+{
+    internal static class TypeHelper
+    {
+        public static object GetDefaultValue(this Type type)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+
+            return type.IsValueType ? Activator.CreateInstance(type) : null;
+        }
+    }
+}
