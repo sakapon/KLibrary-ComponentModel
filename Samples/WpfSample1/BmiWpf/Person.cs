18b3b9a12109e74b266676e5f2b8833ae29702ef
Keiho Sakapon <github@saka-pon.net>
2014-02-17 19:28:39 +0900

    Install KLibrary.ComponentModel.

---
+++ Samples/WpfSample1/BmiWpf/Person.cs
@@ -0,0 +1,44 @@
+using KLibrary.ComponentModel;
+using System;
+using System.ComponentModel;
+
+namespace BmiWpf
+{
+    // An usage sample.
+    public class Person : NotifyBase
+    {
+        [DefaultValue(-1)]
+        public int Id
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+        [DefaultValue("")]
+        public string FirstName
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        [DefaultValue("")]
+        public string LastName
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        [DependentOn("FirstName")]
+        [DependentOn("LastName")]
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName).Trim(); }
+        }
+
+        public DateTime Birthday
+        {
+            get { return GetValue<DateTime>(); }
+            set { SetValue(value); }
+        }
+    }
+}
