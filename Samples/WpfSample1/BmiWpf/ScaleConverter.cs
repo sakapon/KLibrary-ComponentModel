dfbf78e65a1b2e23f82b67f08b4d52413a4216a6
Keiho Sakapon <github@saka-pon.net>
2014-02-17 20:13:03 +0900

    Add ScaleConverter class.

---
+++ Samples/WpfSample1/BmiWpf/ScaleConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BmiWpf
+{
+    [ValueConversion(typeof(double), typeof(double))]
+    public class ScaleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var scale = GetScale(parameter);
+            return
+                (value is double) ? scale * (double)value :
+                (value is int) ? scale * (int)value :
+                DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var scale = GetScale(parameter);
+            return
+                (value is double) ? (double)value / scale :
+                (value is int) ? (int)value / scale :
+                DependencyProperty.UnsetValue;
+        }
+
+        static readonly Func<object, double> GetScale = o =>
+        {
+            try
+            {
+                return System.Convert.ToDouble(o);
+            }
+            catch (Exception)
+            {
+                return 1.0;
+            }
+        };
+    }
+}
