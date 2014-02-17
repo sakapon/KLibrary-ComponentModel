efad7a2738cc9fcee8a772937b9cf8fb02ae8003
Keiho Sakapon <github@saka-pon.net>
2014-02-17 18:18:14 +0900

    Delete unused namespaces.

--- KLibrary2_1/ComponentModel/ComponentModel/NotifyBase.cs
+++ KLibrary2_1/ComponentModel/ComponentModel/NotifyBase.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace KLibrary.ComponentModel
