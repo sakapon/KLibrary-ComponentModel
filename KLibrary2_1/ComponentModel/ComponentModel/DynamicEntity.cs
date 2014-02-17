267fe4f3bebb18710e73e18b686b8a33199b2e6f
Keiho Sakapon <github@saka-pon.net>
2014-02-17 17:38:03 +0900

    Add DynamicEntity class.

---
+++ KLibrary2_1/ComponentModel/ComponentModel/DynamicEntity.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+
+namespace KLibrary.ComponentModel
+{
+    /// <summary>
+    /// Represents the observable object for the dynamic programming.
+    /// </summary>
+    public class DynamicEntity : DynamicObject, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Gets the dictionary which contains the property values of this object.
+        /// </summary>
+        /// <value>The dictionary which contains the property values of this object.</value>
+        protected Dictionary<string, object> PropertyValues { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DynamicEntity"/> class.
+        /// </summary>
+        public DynamicEntity()
+        {
+            PropertyValues = new Dictionary<string, object>();
+        }
+
+        protected bool GetValue(string propertyName, out object result)
+        {
+            if (!PropertyValues.ContainsKey(propertyName))
+            {
+                result = null;
+                return false;
+            }
+
+            result = PropertyValues[propertyName];
+            return true;
+        }
+
+        protected bool SetValue(string propertyName, object value)
+        {
+            if (PropertyValues.ContainsKey(propertyName) && object.Equals(PropertyValues[propertyName], value)) return true;
+
+            PropertyValues[propertyName] = value;
+            NotifyPropertyChanged(propertyName);
+            return true;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return GetValue(binder.Name, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            return SetValue(binder.Name, value);
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            if (indexes == null || indexes.Length < 1 || !(indexes[0] is string))
+            {
+                result = null;
+                return false;
+            }
+            return GetValue((string)indexes[0], out result);
+        }
+
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            if (indexes == null || indexes.Length < 1 || !(indexes[0] is string))
+            {
+                return false;
+            }
+            return SetValue((string)indexes[0], value);
+        }
+
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return PropertyValues.Keys;
+        }
+
+        /// <summary>
+        /// Occurs when a property value has changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (o, e) => { };
+
+        /// <summary>
+        /// Notifies that the property value has changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property which has changed.</param>
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Add the action which is executed when the property has changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property which has changed.</param>
+        /// <param name="action">The action which is executed when the property has changed.</param>
+        public void AddPropertyChangedHandler(string propertyName, Action action)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+
+            PropertyChanged += (o, e) =>
+            {
+                if (e.PropertyName == propertyName)
+                {
+                    action();
+                }
+            };
+        }
+    }
+}
---
+++ KLibrary2_1/UnitTest/ComponentModel/DynamicEntityTest.cs
@@ -0,0 +1,98 @@
+﻿using KLibrary.ComponentModel;
+using Microsoft.CSharp.RuntimeBinder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest.ComponentModel
+{
+    [TestClass]
+    public class DynamicEntityTest
+    {
+        [TestMethod]
+        public void Person_1()
+        {
+            var idNotifiedCount = 0;
+            var firstNameNotifiedCount = 0;
+
+            dynamic target = new DynamicEntity();
+            target.Id = -1;
+            target.AddPropertyChangedHandler("Id", new Action(() => { idNotifiedCount++; }));
+            target.AddPropertyChangedHandler("FirstName", new Action(() => { firstNameNotifiedCount++; }));
+
+            target.Id = -1;
+            target.FirstName = "Ichiro";
+
+            Assert.AreEqual(-1, target.Id);
+            Assert.AreEqual("Ichiro", target.FirstName);
+            Assert.AreEqual(0, idNotifiedCount);
+            Assert.AreEqual(1, firstNameNotifiedCount);
+
+            target.Id = 123;
+            target.FirstName = "Jiro";
+
+            Assert.AreEqual(123, target.Id);
+            Assert.AreEqual("Jiro", target.FirstName);
+            Assert.AreEqual(1, idNotifiedCount);
+            Assert.AreEqual(2, firstNameNotifiedCount);
+
+            target["Id"] = 456;
+            target["FirstName"] = "Saburo";
+
+            Assert.AreEqual(456, target.Id);
+            Assert.AreEqual("Saburo", target.FirstName);
+            Assert.AreEqual(456, target["Id"]);
+            Assert.AreEqual("Saburo", target["FirstName"]);
+            Assert.AreEqual(2, idNotifiedCount);
+            Assert.AreEqual(3, firstNameNotifiedCount);
+        }
+
+        [TestMethod]
+        public void GetDynamicMemberNames_1()
+        {
+            dynamic target = new DynamicEntity();
+            target.Id = null;
+            target.Name = null;
+            CollectionAssert.AreEquivalent(new[] { "Id", "Name" }, (System.Collections.ICollection)target.GetDynamicMemberNames());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RuntimeBinderException))]
+        public void TryGetMember_NotContains()
+        {
+            dynamic target = new DynamicEntity();
+            var id = target.Id;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RuntimeBinderException))]
+        public void TryGetIndex_NotContains()
+        {
+            dynamic target = new DynamicEntity();
+            var id = target["Id"];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RuntimeBinderException))]
+        public void TryGetIndex_InvalidIndex()
+        {
+            dynamic target = new DynamicEntity();
+            var id = target[1];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RuntimeBinderException))]
+        public void TrySetIndex_InvalidIndex()
+        {
+            dynamic target = new DynamicEntity();
+            target[1] = 123;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddPropertyChangedHandler_Null()
+        {
+            dynamic target = new DynamicEntity();
+            target.AddPropertyChangedHandler("", null);
+        }
+    }
+}
