3619d2f0807e68a713ede9b13c073a8a436352b7
Keiho Sakapon <github@saka-pon.net>
2014-02-22 12:33:27 +0900

    Add DynamicValidEntity class.

---
+++ KLibrary2_1/ComponentModel/ComponentModel/DynamicValidEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace KLibrary.ComponentModel
+{
+    public class DynamicValidEntity : DynamicEntity, INotifyDataErrorInfo
+    {
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public System.Collections.IEnumerable GetErrors(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasErrors
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
