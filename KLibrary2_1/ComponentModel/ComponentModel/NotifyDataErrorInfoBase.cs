2e9555fb4f845ea19b2f7763d65018d549e5ab61
Keiho Sakapon <github@saka-pon.net>
2014-02-24 20:13:53 +0900

    Add base classes for interfaces to notify.

---
+++ KLibrary2_1/ComponentModel/ComponentModel/NotifyDataErrorInfoBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace KLibrary.ComponentModel
+{
+    /// <summary>
+    /// Provides the base implementation for the <see cref="INotifyPropertyChanged"/> and <see cref="INotifyDataErrorInfo"/> interfaces.
+    /// </summary>
+    public abstract class NotifyDataErrorInfoBase : NotifyPropertyChangedBase, INotifyDataErrorInfo
+    {
+        /// <summary>
+        /// Occurs when the validation errors have changed for a property or for the entire entity.
+        /// </summary>
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged = (o, e) => { };
+
+        /// <summary>
+        /// Notifies that the validation errors have changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property; or null or String.Empty for entity-level errors.</param>
+        public virtual void NotifyErrorsChanged([CallerMemberName]string propertyName = "")
+        {
+            ErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Add the action which is executed when the validation errors have changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property; or null or String.Empty for entity-level errors.</param>
+        /// <param name="action">The action which is executed when the validation errors have changed.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="action"/> is <see langword="null"/>.</exception>
+        public virtual void AddErrorsChangedHandler(string propertyName, Action action)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+
+            ErrorsChanged += (o, e) =>
+            {
+                if (e.PropertyName == propertyName)
+                {
+                    action();
+                }
+            };
+        }
+
+        /// <summary>
+        /// Gets the validation errors for a specified property or for the entire entity.
+        /// </summary>
+        /// <param name="propertyName">The name of the property to retrieve validation errors for; or null or String.Empty, to retrieve entity-level errors.</param>
+        /// <returns>The validation errors for the property or entity.</returns>
+        public abstract System.Collections.IEnumerable GetErrors(string propertyName);
+
+        /// <summary>
+        /// Gets a value that indicates whether the entity has validation errors.
+        /// </summary>
+        /// <value>true if the entity currently has validation errors; otherwise, false.</value>
+        public abstract bool HasErrors { get; }
+    }
+}
---
+++ KLibrary2_1/ComponentModel/ComponentModel/NotifyPropertyChangedBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace KLibrary.ComponentModel
+{
+    /// <summary>
+    /// Provides the base implementation for the <see cref="INotifyPropertyChanged"/> interface.
+    /// </summary>
+    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property value has changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (o, e) => { };
+
+        /// <summary>
+        /// Notifies that the property value has changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property which has changed.</param>
+        public virtual void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Add the action which is executed when the property has changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property which has changed.</param>
+        /// <param name="action">The action which is executed when the property has changed.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="action"/> is <see langword="null"/>.</exception>
+        public virtual void AddPropertyChangedHandler(string propertyName, Action action)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+
+            PropertyChanged += (o, e) =>
+            {
+                if (e.PropertyName == propertyName)
+                {
+                    action();
+                }
+            };
+        }
+    }
+}
